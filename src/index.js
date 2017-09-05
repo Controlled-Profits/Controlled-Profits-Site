931496a04b7dd0f8644213fc35b4203bdd606b74
Robert Hutchins <rjhutchins22@gmail.com>
2017-09-05 15:57:03 -0400

    React routered that stuff

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Redirect, BrowserRouter, Route, Switch } from 'react-router-dom';
+import MemberHomeBox from './components/memberHome/memberHomeBox.js';
+import BusinessHome from './components/memberHome/businessFinancials.js';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <BrowserRouter>
+    <Switch>
+      <Route path='/member/:user' component={MemberHomeBox}/>
+      <Route path='/member/:user/:buisness' component={BusinessHome}/>
+      <Route exact path='/' component={App} />
+    </Switch>
+  </BrowserRouter> ,
+  document.getElementById('root'));
 registerServiceWorker();
