1ce2a88f686d74c06b4344c54b0ed73282c8b480
Robert Hutchins <rjhutchins22@gmail.com>
2017-09-07 20:52:37 -0400

    woking on building out components for financial form/input

---
+++ src/components/financial/simpleSum.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+export default class SimpleSum extends Component{
+  constructor(props){
+    super(props);
+  }
+
+  render(){
+    return(
+      <div>
+        <p>Simpsum</p>
+      </div>
+    )
+  }
+}
