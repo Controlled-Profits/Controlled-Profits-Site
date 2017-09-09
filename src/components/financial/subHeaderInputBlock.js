280c0756e45a52dcbdda205b83bbcbc4d17ee741
Robert Hutchins <rjhutchins22@gmail.com>
2017-09-09 10:10:27 -0400

    split apart nested map to actuall coomponents

--- src/components/financial/subHeaderInputBlock.js
+++ src/components/financial/subHeaderInputBlock.js
@@ -1,14 +1,31 @@
 import React, {Component} from 'react';
+import FinancialInput from './inputFinance.js';
 
-export default class SimpleSum extends Component{
+export default class SubHeaderInputBlock extends Component{
   constructor(props){
     super(props);
+    this.state = {
+      subSectionInput:[]
+    }
+  }
+
+  componentDidMount(){
+    let subHeaderRes = this.props.subHeaderInput;
+    console.log('subHeaderRes:', subHeaderRes);
+    let subHeaderResCards = subHeaderRes.map((field)=> {
+      return(
+        <div>
+        <FinancialInput title={field.title} currentValue={field.value}/>
+        </div>
+      )
+    });
+    this.setState({subSectionInput: subHeaderResCards});
   }
 
   render(){
     return(
       <div>
-        <p>Simpsum</p>
+        {this.state.subSectionInput}
       </div>
     )
   }
